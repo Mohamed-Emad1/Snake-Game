ab70e9a2d89e16dd530ecf51102d6c6c5643deb8
Mohamed Emad <mohamed.emad45621@gmail.com>
2023-08-11 02:08:10 +0300

    add second screen

--- main.py
+++ main.py
@@ -1,48 +1,77 @@
+import tkinter
 from snake import Snake
 from turtle import Screen
 import time
 from food import Food
 from score import ScoreBoard
+from tkinter import Tk, Label, Button, PhotoImage
+
+import ctypes
+my_app = 'mycompany.myproduct.subproduct.version'   # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app)
 
 
-screen = Screen()
-screen.bgpic("snake.gif")
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
+window = Tk()
+window.geometry('850x542')
+window.resizable(False,False)           # make window fixed size
+window.title("Snake Game")
+bg = PhotoImage(file="ka.gif")
 
-snake = Snake()   # object from snake
-food = Food()
-score = ScoreBoard()
+label1 = Label(window, image=bg)
+label1.place(x=0, y=0)
 
-
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.left, "Left")
+label = Label(window, text="Welcome to Snake Game Press Play to start the game", fg="cyan", bg="black",
+              font=("Cairo", 16, "bold"))
+label.pack(side=tkinter.TOP, pady=5)
 
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.07)
-    snake.move()
+def start_game():
+    window.destroy()
+    screen = Screen()
+    screen.bgpic("snake.gif")
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.tracer(0)
 
-    if snake.head.distance(food) < 15:
-        score.increase_score()
-        snake.extended_snake()
-        food.refresh()
-    if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 295 or snake.head.ycor() < -290:
-        game_is_on = False
-        screen.reset()
-        score.end_game()
+    snake = Snake()   # object from snake
+    food = Food()
+    score = ScoreBoard()
 
-    for square in snake.new_box[1:]:
-        if snake.head.distance(square) < 10:
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.right, "Right")
+    screen.onkey(snake.left, "Left")
+
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.07)
+        snake.move()
+
+        if snake.head.distance(food) < 15:
+            score.increase_score()
+            snake.extended_snake()
+            food.refresh()
+        if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 295 or snake.head.ycor() < -290:
             game_is_on = False
             screen.reset()
             score.end_game()
 
-screen.exitonclick()
+        for square in snake.new_box[1:]:
+            if snake.head.distance(square) < 10:
+                game_is_on = False
+                screen.reset()
+                score.end_game()
+
+    screen.exitonclick()
+
+
+b = Button(window, text="Play", command=start_game, height=3, width=10, font=("Cairo", 16, "bold"),
+           fg="yellow", bg="black")
+
+b.pack(side=tkinter.BOTTOM, pady=30)
+
+window.iconbitmap('snake.ico')
+window.mainloop()
