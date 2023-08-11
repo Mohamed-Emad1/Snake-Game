907dcedeb23ce6673dd878b509b5a3efc9436a85
Mohamed Emad <mohamed.emad45621@gmail.com>
2023-08-11 17:07:47 +0300

    music files

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import tkinter
+
+import pygame
+
 from snake import Snake
 from turtle import Screen
 import time
 from food import Food
 from score import ScoreBoard
 from tkinter import Tk, Label, Button, PhotoImage
+from pygame import mixer
 
 import ctypes
 my_app = 'mycompany.myproduct.subproduct.version'   # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app)
-
+pygame.init()
 
 window = Tk()
 window.geometry('850x542')
 window.resizable(False,False)           # make window fixed size
 window.title("Snake Game")
 bg = PhotoImage(file="ka.gif")
+
+mixer.music.load("background.wav")
+mixer.music.play(-1)
 
 label1 = Label(window, image=bg)
 label1.place(x=0, y=0)
@@ -51,17 +58,25 @@ def start_game():
         snake.move()
 
         if snake.head.distance(food) < 15:
+            food_sound = mixer.Sound("eating.wav")
+            food_sound.play()
             score.increase_score()
             snake.extended_snake()
             food.refresh()
         if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 295 or snake.head.ycor() < -290:
             game_is_on = False
+            pygame.mixer.music.set_volume(0)
+            game_over = mixer.Sound("gameover.mp3")
+            game_over.play()
             screen.reset()
             score.end_game()
 
         for square in snake.new_box[1:]:
             if snake.head.distance(square) < 10:
                 game_is_on = False
+                pygame.mixer.music.set_volume(0)
+                game_over = mixer.Sound("gameover.mp3")
+                game_over.play()
                 screen.reset()
                 score.end_game()
 
